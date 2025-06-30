0d39b49893b5922e8a598e90844dadbb162033d4
infix123 <adediran.oludare@gmail.com>
2025-06-30 06:44:56 -0400

    Using promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,32 +29,65 @@ public_users.post("/register", (req, res) => {
         // Check if the user does not already exist
         if (!doesExist(username)) {
             // Add the new user to the users array
-            users.push({"username": username, "password": password});
-            return res.status(200).json({message: "User successfully registered. Now you can login"});
+            users.push({ "username": username, "password": password });
+            return res.status(200).json({ message: "User successfully registered. Now you can login" });
         } else {
-            return res.status(404).json({message: "User already exists!"});
+            return res.status(404).json({ message: "User already exists!" });
         }
     }
     // Return error if username or password is missing
-    return res.status(404).json({message: "Unable to register user."});
+    return res.status(404).json({ message: "Unable to register user." });
 
-  
+
 });
+
+
+//Promise that return all books
+function getBooks(books) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                // Assuming books is an Object
+                if (!books) {
+                    throw new Error("Input must be an object of books");
+                }
+                resolve(books);
+            } catch (error) {
+                reject(error); // Reject the promise in case of an error
+            }
+        }, 1000);
+    });
+}
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-    //Write your code here
-    res.send(books);
-    return res.status(300).json({ message: "Yet to be implemented" });
+    getBooks(books)
+        .then(books => {
+            return res.status(200).json({ message: "Books returned successfully", books });
+        })
+        .catch(error => {
+            console.error("Error:", error.message);
+        });
+
 });
+
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     //Write your code here
     const isbn = req.params.isbn;
-    let getbook = books[isbn];
-    res.send(getbook);
-    return res.status(300).json({ message: "Yet to be implemented" });
+
+    // res.send(getbook);
+    getBooks(books)
+        .then(books => {
+            let getbookByISBN = books[isbn];
+            return res.status(200).json({ message: "Books returned successfully", getbookByISBN });
+        })
+        .catch(error => {
+            console.error("Error:", error.message);
+        });
+
 });
 
 // Get book details based on author
@@ -62,14 +95,21 @@ public_users.get('/author/:author', function (req, res) {
     //Write your code here
     const authorname = req.params.author;
     let book = []
-    for (let key in books) {
-        if (books[key].author === authorname) {
-          book.push(books[key])          
-        }
-    }
-    res.send(book);
+    getBooks(books)
+        .then(books => {
+            for (let key in books) {
+                if (books[key].author === authorname) {
+                    book.push(books[key])
+                }
+            }
+            return res.status(200).json({ message: "Books returned successfully", book });
+        })
+        .catch(error => {
+            console.error("Error:", error.message);
+        });
 
-    return res.status(300).json({ message: "Yet to be implemented" });
+
+
 });
 
 // Get all books based on title
@@ -77,13 +117,19 @@ public_users.get('/title/:title', function (req, res) {
     //Write your code here
     const title = req.params.title;
     let book = []
-    for (let key in books) {
-        if (books[key].title === title) {
-          book.push(books[key])          
-        }
-    }
-    res.send(book);
-    return res.status(300).json({ message: "Yet to be implemented" });
+    getBooks(books)
+        .then(books => {
+            for (let key in books) {
+                if (books[key].title === title) {
+                    book.push(books[key])
+                }
+            }
+            return res.status(200).json({ message: "Books returned successfully", book });
+        })
+        . catch(error => {
+            console.error("Error:", error.message);
+        });
+   
 });
 
 //  Get book review
